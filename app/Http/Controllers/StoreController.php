6610f29ee05349f0b22788c5c12c6acee335d753
Kevin <89836835+Kaasstengel28@users.noreply.github.com>
2022-10-29 16:34:29 +0200

    zoekbalk + crud still broken i dont know how to fix it please help

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -7,35 +7,25 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function index() {
-        $products = Product::all();
-        return view('store.storefront')->with('products', $products);
+    public function index(Request $request)
+    {
+        $products = Product::where([
+        ['title', '!=', Null],
+        [function ($query) use ($request) {
+            if (($term = $request->term)) {
+                $query->orWhere('title', 'LIKE', '%' . $term . '%')->get();
+                }
+            }]
+        ])
+            ->orderby("id", "desc")
+            ->paginate(10);
+
+        return view('store.storefront', compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     public function show($id) {
         $product = Product::find($id);
         return view('store.show', compact('product'));
     }
-
-    public function store(Request $request){
-        //Merge request to data
-        $data = $request->all();
-        $request->merge($data);
-
-        //Validate request
-        $this->validate($request,
-            [
-                'title' => 'bail|required|unique:products|max:255',
-                'price' => 'bail|required|numeric',
-                'description' => 'nullable'
-            ]);
-        //Add and redirect
-        Product::create($request->all());
-        return redirect('/products');
-    }
-
-    public function delete () {
-        return view('product.delete');
-    }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutmeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
